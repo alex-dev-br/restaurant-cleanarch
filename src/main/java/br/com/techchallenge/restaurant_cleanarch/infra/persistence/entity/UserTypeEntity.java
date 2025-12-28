2443ff333cf18f8a8bc133050a4862d7cb15fd0e
Kervin Candido <kervincandido@gmail.com>
2025-12-28 12:38:43 -0300

    fix(userType): corrigindo id do tipo de usuario
    corrigindo id do tipo de usuario de uuid para id

--- src/main/java/br/com/techchallenge/restaurant_cleanarch/infra/persistence/entity/UserTypeEntity.java
+++ src/main/java/br/com/techchallenge/restaurant_cleanarch/infra/persistence/entity/UserTypeEntity.java
@@ -1,16 +1,17 @@
 package br.com.techchallenge.restaurant_cleanarch.infra.persistence.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
-
-import java.util.UUID;
 
 @Entity
 @Data
 public class UserTypeEntity {
     @Id
     @GeneratedValue
-    private UUID id;
+    private Long id;
     @Column(nullable = false)
     private String name;
 }
--- src/main/java/br/com/techchallenge/restaurant_cleanarch/infra/persistence/repository/UserTypeRepository.java
+++ src/main/java/br/com/techchallenge/restaurant_cleanarch/infra/persistence/repository/UserTypeRepository.java
@@ -3,7 +3,5 @@ package br.com.techchallenge.restaurant_cleanarch.infra.persistence.repository;
 import br.com.techchallenge.restaurant_cleanarch.infra.persistence.entity.UserTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface UserTypeRepository extends JpaRepository<UserTypeEntity, UUID> {
+public interface UserTypeRepository extends JpaRepository<UserTypeEntity, Long> {
 }
