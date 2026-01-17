57097a397b12acb65c7a65f1d08d1029b91a7149
Kervin Candido <kervincandido@gmail.com>
2026-01-17 00:24:16 -0300

    infra(flyway): config flyway
    configurando flyway

--- src/main/resources/db/migration/V2__INIT_DATA.sql
+++ src/main/resources/db/migration/V2__INIT_DATA.sql
@@ -17,11 +17,17 @@ INSERT INTO roles (name) VALUES ('UPDATE_USER');
 INSERT INTO roles (name) VALUES ('DELETE_USER');
 INSERT INTO roles (name) VALUES ('VIEW_USER');
 
+INSERT INTO roles (name) VALUES ('CREATE_MENU_ITEM');
+INSERT INTO roles (name) VALUES ('UPDATE_MENU_ITEM');
+INSERT INTO roles (name) VALUES ('DELETE_MENU_ITEM');
+INSERT INTO roles (name) VALUES ('VIEW_MENU_ITEM');
+
 INSERT INTO user_types (name) VALUES ('RESTAURANT_OWNER');
+INSERT INTO user_types (name) VALUES ('CUSTOMER');
 
 -- associa todas as roles ao user type RESTAURANT_OWNER
 INSERT INTO user_types_roles (user_type_id, role_id)
-SELECT ut.id, r.id
-FROM user_types ut
-         JOIN roles r ON 1=1
-WHERE ut.name = 'RESTAURANT_OWNER';
+    SELECT ut.id, r.id FROM user_types ut JOIN roles r ON 1=1 WHERE ut.name = 'RESTAURANT_OWNER';
+
+INSERT INTO user_types_roles (user_type_id, role_id)
+SELECT ut.id, r.id FROM user_types ut JOIN roles r ON 1=1 WHERE ut.name = 'CUSTOMER' AND r.name in ('VIEW_RESTAURANT', 'VIEW_MENU_ITEM');
