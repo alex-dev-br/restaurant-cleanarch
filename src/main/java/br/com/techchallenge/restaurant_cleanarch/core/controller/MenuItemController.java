425a2c545a8b6c154d6a194c528e237e221799da
Kervin Candido <kervincandido@gmail.com>
2026-01-18 13:10:23 -0300

    feature(Menu): implementando alteração de item de menu
     implementando alteração de item de menu

--- src/main/java/br/com/techchallenge/restaurant_cleanarch/core/controller/MenuItemController.java
+++ src/main/java/br/com/techchallenge/restaurant_cleanarch/core/controller/MenuItemController.java
@@ -3,10 +3,12 @@ package br.com.techchallenge.restaurant_cleanarch.core.controller;
 import br.com.techchallenge.restaurant_cleanarch.core.domain.pagination.Page;
 import br.com.techchallenge.restaurant_cleanarch.core.domain.pagination.PagedQuery;
 import br.com.techchallenge.restaurant_cleanarch.core.inbound.CreateMenuItemInput;
+import br.com.techchallenge.restaurant_cleanarch.core.inbound.UpdateMenuItemInput;
 import br.com.techchallenge.restaurant_cleanarch.core.outbound.MenuItemOutput;
 import br.com.techchallenge.restaurant_cleanarch.core.presenter.MenuItemPresenter;
 import br.com.techchallenge.restaurant_cleanarch.core.usecase.menuitem.CreateMenuItemUseCase;
 import br.com.techchallenge.restaurant_cleanarch.core.usecase.menuitem.ListMenuItemsByRestaurantUseCase;
+import br.com.techchallenge.restaurant_cleanarch.core.usecase.menuitem.UpdateMenuItemUseCase;
 
 import java.util.Objects;
 
@@ -14,12 +16,15 @@ public class MenuItemController {
 
     private final ListMenuItemsByRestaurantUseCase listMenuItemsByRestaurantUseCase;
     private final CreateMenuItemUseCase createMenuItemUseCase;
+    private final UpdateMenuItemUseCase updateMenuItemUseCase;
 
-    public MenuItemController(ListMenuItemsByRestaurantUseCase listMenuItemsByRestaurantUseCase, CreateMenuItemUseCase createMenuItemUseCase) {
+    public MenuItemController(ListMenuItemsByRestaurantUseCase listMenuItemsByRestaurantUseCase, CreateMenuItemUseCase createMenuItemUseCase, UpdateMenuItemUseCase updateMenuItemUseCase) {
         Objects.requireNonNull(listMenuItemsByRestaurantUseCase, "ListMenuItemsByRestaurantUseCase cannot be null.");
         Objects.requireNonNull(createMenuItemUseCase, "CreateMenuItemUseCase cannot be null.");
+        Objects.requireNonNull(updateMenuItemUseCase, "UpdateMenuItemUseCase cannot be null.");
         this.listMenuItemsByRestaurantUseCase = listMenuItemsByRestaurantUseCase;
         this.createMenuItemUseCase = createMenuItemUseCase;
+        this.updateMenuItemUseCase = updateMenuItemUseCase;
     }
 
     public Page<MenuItemOutput> findByRestaurant(Long restaurantId, int pageNumber, int pageSize) {
@@ -31,5 +36,9 @@ public class MenuItemController {
     public MenuItemOutput addItemInMenu(CreateMenuItemInput input) {
         var menuItem = createMenuItemUseCase.execute(input);
         return MenuItemPresenter.toOutput(menuItem, input.restaurantId());
+    }
+
+    public void updateMenuItem(UpdateMenuItemInput input) {
+        updateMenuItemUseCase.execute(input);
     }
 }
